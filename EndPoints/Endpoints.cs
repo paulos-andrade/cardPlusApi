3cb5b1f6b1ab212776ad54dd6f9684343a528bc6
Schoolmakoto <paulosousa.andradegmail.com>
2023-01-30 20:25:53 -0300

    Moved EndPoints to an extension method.

---
+++ EndPoints/Endpoints.cs
@@ -0,0 +1,43 @@
+﻿using cadPlus_Api.Data;
+using cadPlus_Api.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EndPoints
+{
+
+    public static class EndpointsList
+    {
+        public static void RegisterUsersApis (this WebApplication app)
+        {
+
+            app.MapGet("v1/getAllUsers", async (ApplicationDbContext context) =>
+            {
+                var users = await context.Users.ToListAsync();
+                return Results.Ok(users);
+
+            }).Produces<User>(); // retorna o Schema do user
+
+            app.MapGet("v1/getUser/{id}", async (int id, ApplicationDbContext context) =>
+            {
+                var users = await context.Users.FirstOrDefaultAsync(x => x.Id == id);
+                return Results.Ok(users);
+
+            }).Produces<User>();
+
+            app.MapPost("v1/insertUser", async (
+                ApplicationDbContext context, CreateUserViewModel userModel) =>
+            {
+                var user = userModel.Validate();
+
+                if (userModel.IsValid)
+                {
+                    context.Users.Add(user);
+                    await context.SaveChangesAsync();
+                    return Results.Created($"/v1/insertUser/{user.Id}", user);
+                }
+                return Results.BadRequest(userModel.Notifications);
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,41 +1,14 @@
 using cadPlus_Api.Data;
-using cadPlus_Api.ViewModels;
-using Microsoft.EntityFrameworkCore;
+using EndPoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>();
-//Configurações do banco (connection string,etc)
+
 
 var app = builder.Build();
+app.RegisterUsersApis();
 
 
-app.MapGet("v1/getAllUsers", async (ApplicationDbContext context) =>
-{
-    var users = await context.Users.ToListAsync();
-    return Results.Ok(users);
-
-}).Produces<User>(); // retorna o Schema do user
-
-app.MapGet("v1/getUser/{id}", async (int id, ApplicationDbContext context) =>
-{
-    var users = await context.Users.FirstOrDefaultAsync(x => x.Id == id);
-    return Results.Ok(users);
-
-}).Produces<User>();
-
-app.MapPost("v1/insertUser", async (
-    ApplicationDbContext context, CreateUserViewModel userModel) =>
-{
-    var user = userModel.Validate();
-
-    if (userModel.IsValid)
-    {
-        context.Users.Add(user);
-        await context.SaveChangesAsync();
-        return Results.Created($"/v1/insertUser/{user.Id}", user);
-    }
-    return Results.BadRequest(userModel.Notifications);
-});
 
 app.Run();
