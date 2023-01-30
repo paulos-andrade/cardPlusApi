80264b61777959f3a95851ca554184b073d86242
Schoolmakoto <paulosousa.andradegmail.com>
2023-01-30 19:49:20 -0300

    Changes to Async started, most endpoints done.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using cadPlus_Api.Data;
 using cadPlus_Api.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,34 +9,33 @@ builder.Services.AddDbContext<ApplicationDbContext>();
 
 var app = builder.Build();
 
-app.MapGet("v1/getAllUsers", (ApplicationDbContext context) =>
-{
-    var users = context.Users.ToList();
-    return Results.Ok(users);
-});
 
-app.MapGet("v1/getUser:{id}", (ApplicationDbContext context) =>
+app.MapGet("v1/getAllUsers", async (ApplicationDbContext context) =>
 {
-    var users = context.Users.ToList();
+    var users = await context.Users.ToListAsync();
     return Results.Ok(users);
-});
 
-app.MapPost("v1/insertUser", (
+}).Produces<User>(); // retorna o Schema do user
+
+app.MapGet("v1/getUser/{id}", async (int id, ApplicationDbContext context) =>
+{
+    var users = await context.Users.FirstOrDefaultAsync(x => x.Id == id);
+    return Results.Ok(users);
+
+}).Produces<User>();
+
+app.MapPost("v1/insertUser", async (
     ApplicationDbContext context, CreateUserViewModel userModel) =>
 {
     var user = userModel.Validate();
 
-    if (!userModel.IsValid)
+    if (userModel.IsValid)
     {
-        return Results.BadRequest(userModel.Notifications);
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+        return Results.Created($"/v1/insertUser/{user.Id}", user);
     }
-    context.Users.Add(user);
-    context.SaveChanges();
-    return Results.Created($"/v1/insertUser/{user.Id}", user);
+    return Results.BadRequest(userModel.Notifications);
 });
-
-
-app.MapPost("/", () => "Cad User");
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
